3284c834bb84c7611b030627784ac2a91fae6514
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2020-11-12 13:40:25 +0100

    add recursive_format method

--- snakemk_util/__init__.py
+++ snakemk_util/__init__.py
@@ -1,1 +1,2 @@
 from .rule_args import load_rule_args
+from .formatting import recursive_format
---
+++ snakemk_util/formatting.py
@@ -0,0 +1,14 @@
+
+
+def recursive_format(data, params, fail_on_unknown=False):
+    if isinstance(data, str):
+        return data.format(**params)
+    elif isinstance(data, dict):
+        return {k: recursive_format(v, params) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [recursive_format(v, params) for v in data]
+    else:
+        if fail_on_unknown:
+            raise ValueError("Handling of data type not implemented: %s" % type(data))
+        else:
+            return data
