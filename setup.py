f8cc7e232fb218883e9ee89f211a02bd2c85ec58
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2020-11-18 16:25:37 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ dev_requirements = [
 
 setup(
     name='snakemk_util',
-    version='0.1.0',
+    version='0.2.0',
     description="utility functions for snakemake",
     author="Florian Hölzlwimmer",
     author_email='git.ich@frhoelzlwimmer.de',
