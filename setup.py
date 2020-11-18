9967d16defb059ec4719bb2a6c0eafc965a18db4
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2020-11-18 16:27:47 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ dev_requirements = [
 
 setup(
     name='snakemk_util',
-    version='0.2.0',
+    version='0.2.1',
     description="utility functions for snakemake",
     author="Florian Hölzlwimmer",
     author_email='git.ich@frhoelzlwimmer.de',
--- snakemk_util/__init__.py
+++ snakemk_util/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .rule_args import load_rule_args
 from .formatting import recursive_format
